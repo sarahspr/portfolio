c1b15f7674a5728b7dab77a301161768d9011130
Sarah Spriggs <sarah1591@gmail.com>
2025-05-01 17:41:30 -0500

    Starts updating structure and styling of project grid section.

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -7,26 +7,18 @@ const ProjectCard = (props) => {
 	let technology = props.technology;
 
 	return (
-		<>
-			<div className="project-card">
-				<a
-					href={href}
-					target="_blank"
-					rel="noreferrer"
-					className="project-card__img-wrapper"
-				>
-					<img
-						src={imgSrc}
-						className="project-card__project-img"
-						alt="screenshot of project homepage"
-					/>
-					<div className="project-card__overlay">
-						<h3 className="h5 project-card__title">{title}</h3>
-						<div className="project-card__technology">{technology}</div>
-					</div>
-				</a>
+		<a
+			href={href}
+			target="_blank"
+			rel="noreferrer"
+			className="project-card"
+			style={{backgroundImage: `url(${imgSrc})`}}
+		>
+			<div className="project-card__overlay">
+				<h3 className="h5 project-card__title">{title}</h3>
+				<div className="project-card__technology">{technology}</div>
 			</div>
-		</>
+		</a>
 	);
 };
 
